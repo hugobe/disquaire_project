8605c2e44cce84e56d6cec093a849539a41a3231
Hugo Pavy <hugo.pavy@gmail.com>
2020-07-09 12:44:43 +0200

    project init

---
+++ store/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Artist(models.Model):
+  name = models.CharField(max_length=200, unique=True)
+
+class Album(models.Model):
+  reference = models.IntegerField(null=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  available = models.BooleanField(default=True)
+  title = models.CharField(max_length=200, unique=True)
+  picture = models.URLField()
+  artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+
+class Contact(models.Model):
+  email = models.EmailField(max_length=100)
+  name = models.CharField(max_length=200, unique=True)
+
+class Booking(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  contacted = models.BooleanField(default=False)
+  contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+  album = models.OneToOneField(Album, on_delete=models.CASCADE )
