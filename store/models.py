a12772890523d43268f3a35eac93db615983d99e
Hugo Pavy <hugo.pavy@gmail.com>
2020-07-11 17:18:32 +0200

    changement

--- store/models.py
+++ store/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Artist(models.Model):
   name = models.CharField(max_length=200, unique=True)
 
+  def __str__(self):
+    return self.name
+
 class Album(models.Model):
   reference = models.IntegerField(null=True)
   created_at = models.DateTimeField(auto_now_add=True)
@@ -11,12 +14,21 @@ class Album(models.Model):
   picture = models.URLField()
   artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
 
+  def __str__(self):
+    return self.title
+
 class Contact(models.Model):
   email = models.EmailField(max_length=100)
   name = models.CharField(max_length=200, unique=True)
+
+  def __str__(self):
+    return self.name
 
 class Booking(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   contacted = models.BooleanField(default=False)
   contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
   album = models.OneToOneField(Album, on_delete=models.CASCADE )
+
+  def __str__(self):
+    return self.contact
